c590b519ae84738f9f211c1d5e77ccfd6f2224fb
helperjby <69462995+helperjby@users.noreply.github.com>
2022-12-22 22:54:02 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    for i in list(numbers):
+        # a = (0+i)
+        a= sum(numbers)
+        b = a / len(numbers)
+    # answer = numbers.sum() / len(numbers)
+    return b
