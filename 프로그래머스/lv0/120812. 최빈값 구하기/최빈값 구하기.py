b9692757c070f931be3c9ff43025bcaaf05c8329
장병용 <helperjby@gmail.com>
2023-01-05 14:51:43 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,14 +1,11 @@
 def solution(array):
     cnt = []
-    # r_array = sorted(array, reverse=True)
     u_cnt = sorted(list(set(array)))
     
     for i in u_cnt:
             cnt.append(array.count(i))
             
-    if len(array) == 1:
-        return (array[0])
-    elif len(u_cnt) == 1:
+    if len(array) == 1 or len(u_cnt) == 1:
         return (array[0])
     elif sorted(cnt)[-1] == sorted(cnt)[-2]:
         return (-1)
