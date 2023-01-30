11dcacb1223dd5410860705ed04b097669d98e05
장병용 <helperjby@gmail.com>
2023-01-30 17:46:46 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    answer = ''
-    my_string = sorted(my_string.lower())
-    for i in my_string:
-        answer += i
-    return answer
+    return ''.join(sorted(my_string.lower()))
