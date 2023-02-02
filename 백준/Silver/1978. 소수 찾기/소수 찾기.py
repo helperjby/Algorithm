7ced6ffa9260e6e83f61c35287edfa2a29b1f34c
장병용 <helperjby@gmail.com>
2023-02-02 11:28:20 +0900

    [Silver V] Title: 소수 찾기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -3,10 +3,11 @@ n = input().split()
 
 sqrt = [2,3,5,7,11,13,17,19,23,29,31] # 1000 이하 제곱근 내 소수
 
+
 nlist = list(range(2,1001)) # 2부터 1000까지
 nprime = [] # 1000 이하의 합성수 리스트
 prime = [] # 1000 이하의 소수 리스트
-answer = [] # 입력된 n 중에 소수 리스트
+answer = 0
 
 # nprime 구하기
 for i in sqrt:
@@ -21,5 +22,5 @@ for i in nlist:
 # answer 구하기
 for i in n:
     if int(i) in prime:
-        answer.append(i)
-print(len(answer))
+        answer += 1
+print (answer)
