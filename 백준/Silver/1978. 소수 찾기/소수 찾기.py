a03b68f4e0a48ec40ce7e3083d179c0d3371cc92
장병용 <helperjby@gmail.com>
2023-02-02 11:29:50 +0900

    [Silver V] Title: 소수 찾기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -5,19 +5,19 @@ sqrt = [2,3,5,7,11,13,17,19,23,29,31] # 1000 이하 제곱근 내 소수
 
 
 nlist = list(range(2,1001)) # 2부터 1000까지
-nprime = [] # 1000 이하의 합성수 리스트
+composite = [] # 1000 이하의 합성수 리스트
 prime = [] # 1000 이하의 소수 리스트
 answer = 0
 
-# nprime 구하기
+# composite 구하기
 for i in sqrt:
     for j in range(2, (1000//i)+1):
-        nprime.append(j*i)
-nrpime = list(set(nprime))
+        composite.append(j*i)
+nrpime = list(set(composite))
 
 # prime 구하기
 for i in nlist:
-    if i not in nprime:
+    if i not in composite:
         prime.append(i)
 # answer 구하기
 for i in n:
